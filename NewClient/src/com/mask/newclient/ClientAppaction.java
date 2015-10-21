a035b0ad4f8a9c82d0bda22f6f45c65a3b57263a
maskA <zhaungmask123@163.com>
2015-10-21 20:55:11 +0800

    1.优化代码

--- NewClient/src/com/mask/newclient/ClientAppaction.java
+++ NewClient/src/com/mask/newclient/ClientAppaction.java
@@ -3,7 +3,9 @@ package com.mask.newclient;
 import android.app.Application;
 
 public class ClientAppaction extends Application{
-	
+	/**
+	 * »ñÈ¡ÆÁÄ»¿í¸ß
+	 */
 	public static int  ScreenWidth = 0;
 	public static int ScreenHeight = 0;
 	
